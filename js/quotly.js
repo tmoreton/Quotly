604f68469e66c4177686f65acaec77f460354bdb
Tim Moreton <tmoreton89@gmail.com>
2015-01-08 11:11:26 -0500

    working on past quote funtionality

--- js/quotly.js
+++ js/quotly.js
@@ -1,3 +1,30 @@
+var csvFile = fs.readFileSync("quotes_fixed.csv","utf8");
+var csv_data = csvParse(csvFile)
+console.log(csv_data);
+
+function csvParse(csvFile){
+    var arrayOfObjects = [];
+    var arr = csvFile.split("\n");
+    var newObj;
+ 
+    keys = arr.shift().split(",");
+ 
+    arr.forEach(function(contact){
+        contact = contact.split(",");
+        newObj = {};
+ 
+        for(var i =0; i < contact.length; i++){
+            newObj[keys[i]] = contact[i];
+        }
+ 
+        arrayOfObjects.push(newObj);
+ 
+    })
+ 
+    return arrayOfObjects;
+}
+
+
 var quotes = [
 "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.  -Helen Keller", 
 "Perfection is not attainable, but if we chase perfection we can catch excellence.  -Vince Lombardi", 
@@ -18,7 +45,6 @@ var quotes = [
 "Your big opportunity may be right where you are now.  -Napoleon Hill",
 "Whoever is happy will make others happy too.  -Anne Frank",
 "When the sun is shining I can do anything; no mountain is too high, no trouble too difficult to overcome.  -Wilma Rudolph",
-"I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.'  -Muhammad Ali"
 ]
 
 var fonts = [
@@ -55,10 +81,11 @@ var fonts = [
 // <!--Digital clock and date function -->
 tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
 tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+var used_quotes= ["I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.'  -Muhammad Ali"];
 
 function GetClock(){
-var d = new Date();
-var nday = d.getDay(),
+  var d = new Date();
+  var nday = d.getDay(),
   nmonth = d.getMonth(),
   ndate = d.getDate(),
   nyear = d.getYear(),
@@ -77,17 +104,16 @@ var nday = d.getDay(),
 
   document.getElementById('clockbox').innerHTML=""+tday[nday]+",  "+tmonth[nmonth]+"  "+ndate+",  "+nyear+"  "+nhour+":"+nmin+ap+"";
 
-  var today = ndate;
-  var used_quotes= [];
-  if ((nhour === 12) && (nmin === 0) && (nsec === 0) && (ap === " AM")) {
+  if ((nhour === 9) && (nmin === 44) && (nsec === 0) && (ap === " AM")) {
       var random_color = '#'+Math.floor(Math.random()*16777215).toString(16);
       var random_num = Math.floor(Math.random()*quotes.length);
       var random_quote = quotes[random_num];
       used_quotes.push(quotes.pop(quotes[random_num]));
       var random_font = fonts[Math.floor(Math.random()*fonts.length)];
       $('body').css('background-color', random_color); 
-      $('h1,p,#clockbox,h2,h3').css('font-family', random_font);
-      $('h2').html("<h2>" + random_quote + "</h2>");
+      $('h1,p,#clockbox,h2,h3,#quote').css('font-family', random_font);
+      document.getElementById('quote').innerHTML = random_quote;
+      //$('h2').html("<h2>" + random_quote + "</h2>");
   }
 }
 
@@ -95,18 +121,18 @@ window.onload=function(){
   GetClock();
   setInterval(GetClock,1000);
 }
-
-
-
-//   $(document).ready(function () {  
-//     $(".btn").click(function() {
-//       var random_color = '#'+Math.floor(Math.random()*16777215).toString(16);
-//       var random_num = Math.floor(Math.random()*quotes.length);
-//       var random_quote = quotes[random_num];
-//         used_quotes.push(quotes.pop(quotes[random_num]));
-//       var random_font = fonts[Math.floor(Math.random()*fonts.length)];
-//       $('body').css('background-color', random_color); 
-//       $('h1,p,#clockbox,h2,h3').css('font-family', random_font);
-//       $('h2').html("<h2>" + random_quote + "</h2>"); 
-//     });
-//   });
+console.log(used_quotes)
+$(document).ready(function () {  
+    $(".btn").click(function() {
+      var random_color = '#'+Math.floor(Math.random()*16777215).toString(16);
+      var random_num = Math.floor(Math.random()*quotes.length);
+      var random_quote = quotes[random_num];
+      used_quotes.push(quotes.pop(quotes[random_num]));
+      var temp_quote = used_quotes[-1];
+      var random_font = fonts[Math.floor(Math.random()*fonts.length)];
+      $('body').css('background-color', random_color); 
+      $('h1,p,#clockbox,h2,h3').css('font-family', random_font);
+      //$('h2').html("<h2>" + temp_quote + "</h2>");
+      document.getElementById('quote').innerHTML = used_quotes[-1];
+    });
+});
